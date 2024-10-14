85e636c59278ed9990029837b34b51035162da94
mdani1201 M <modani1201@gmail.com>
2024-10-14 06:02:08 +0000

    environment

--- myApp.js
+++ myApp.js
@@ -1,41 +1,27 @@
-let express = require('express');
+let express = require("express");
+
+
 let app = express();
 
-console.log("Hello World");
+require('dotenv').config();
 
 
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+app.get("/json", (req, res) => {
+  const hello = "Hello json";
+  const messageStyle = process.env.MESSAGE_STYLE;
+  console.log(messageStyle);
 
+  if (messageStyle === "uppercase") {
+    res.json({ message: hello.toUpperCase() });
+  } else {
+    res.json({ message: hello });
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
